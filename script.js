5dc0c4ff0c809c036c2c281248bff0d5b9402d30
Fallen7240 <99238602+blucloud482@users.noreply.github.com>
2024-10-02 09:13:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let upgradePrice = 10;
 let upgradePrice2 = 50;
 let upgradePrice3 = 150;
 let upgradePrice4 = 250;
+let upgradePrice5 = 500;
+let upgradePrice6 = 1000;
 let totalClicks = 0;
 let upgradesBought = 0;
 
@@ -15,7 +17,10 @@ function updateUI() {
   document.getElementById('upgradePrice2').innerHTML = upgradePrice2.toFixed(0);
   document.getElementById('upgradePrice3').innerHTML = upgradePrice3.toFixed(0);
   document.getElementById('upgradePrice4').innerHTML = upgradePrice4.toFixed(0);
-  document.getElementById('upgradesBought').innerHTML = upgradesBought.toFixed(0);
+  document.getElementById('upgradePrice5').innerHTML = upgradePrice5.toFixed(0);
+  document.getElementById('upgradePrice6').innerHTML = upgradePrice6.toFixed(0);
+  document.getElementById('upgradesBought').innerHTML =
+    upgradesBought.toFixed(0);
   document.getElementById('totalClicks').innerHTML = totalClicks.toFixed(0);
 }
 
@@ -66,6 +71,28 @@ function increaseStonksPerClick4() {
     stonks -= upgradePrice4;
     stonksPerClick += 25;
     upgradePrice4 *= 3;
+    upgradesBought += 1;
+    updateUI();
+  }
+}
+
+//+50
+function increaseStonksPerClick5() {
+  if (stonks >= upgradePrice5) {
+    stonks -= upgradePrice5;
+    stonksPerClick += 50;
+    upgradePrice5 *= 4;
+    upgradesBought += 1;
+    updateUI();
+  }
+}
+
+//*2
+function increaseStonksPerClick6() {
+  if (stonks >= upgradePrice6) {
+    stonks -= upgradePrice6;
+    stonksPerClick *= 2;
+    upgradePrice6 *= 5;
     upgradesBought += 1;
     updateUI();
   }
